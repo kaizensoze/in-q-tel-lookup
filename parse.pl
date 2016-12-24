212397d8fbbb8ab9afe24606cde5bcb82a03aff8
Joe Gallo <joe@brassafrax.com>
2016-12-24 08:26:07 -0500

    update parse script that grabs from portfolio site
    theHarvester has magically stopped working. Either that or linkedin
    profiles are magically being scrubba scrubba dub-dubbed.

--- parse.pl
+++ parse.pl
@@ -2,11 +2,17 @@
 use strict;
 use warnings;
 
-my $url = $ARGV[0] || 'en.wikipedia.org/wiki/In-Q-Tel';
+# primary source: https://www.iqt.org/portfolio/
+# backup source:  https://en.wikipedia.org/wiki/In-Q-Tel
+
+my $url = $ARGV[0] || 'https://www.iqt.org/portfolio/';
 my $regex = $ARGV[1];
 my $contents=`curl -L -s $url`;
 
-my @matches = $contents =~ /\<li\>\<a\ href=".*"\ title=".*"\>(.*?)\<\/a\> (?:–|-)/g;
+my @matches = $contents =~ /\<a class=".*esgbox" href=".*?" lgtitle="(.*?)"\>Read More\<\/a\>/g;
 foreach (@matches) {
     `python /Users/joegallo/dev/theHarvester/theHarvester.py -d "$_" -b linkedin > "$_.txt"`;
 }
+
+# 205 files grabbed via primary
+# 120 files grabbed via backup
