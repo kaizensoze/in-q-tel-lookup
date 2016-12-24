1a8945807e7cc5108e4fe7676362126792e6112e
Joe Gallo <joe@brassafrax.com>
2016-12-24 17:51:04 -0500

    add some more companies

--- parse.pl
+++ parse.pl
@@ -10,8 +10,14 @@ my $regex = $ARGV[1];
 my $contents=`curl -L -s $url`;
 
 my @matches = $contents =~ /\<a class=".*esgbox" href=".*?" lgtitle="(.*?)"\>Read More\<\/a\>/g;
+my $batch_size = 50;
+my $batch_n = 2;
+my $i = $batch_n - 1;
 foreach (@matches) {
-    `python /Users/joegallo/dev/theHarvester/theHarvester.py -d "$_" -b linkedin > "companies/$_.txt"`;
+    if (++$i <= $batch_n*$batch_size) {
+        print "$i\n";
+        `python /Users/joegallo/dev/theHarvester/theHarvester.py -d "$_" -b linkedin > "companies/$_.txt"`;
+    }
 }
 
 # 205 files grabbed via primary
