a2738ce89caf09f26589e2620c581ff740020d41
Joe Gallo <joe@brassafrax.com>
2016-12-24 19:09:37 -0500

    add more companies

--- parse.pl
+++ parse.pl
@@ -10,14 +10,13 @@ my $regex = $ARGV[1];
 my $contents=`curl -L -s $url`;
 
 my @matches = $contents =~ /\<a class=".*esgbox" href=".*?" lgtitle="(.*?)"\>Read More\<\/a\>/g;
-my $batch_size = 50;
-my $batch_n = 2;
-my $i = $batch_n - 1;
+my $i = 0;
 foreach (@matches) {
-    if (++$i <= $batch_n*$batch_size) {
+    if ($i >= 45 && $i <= 90) {
         print "$i\n";
         `python /Users/joegallo/dev/theHarvester/theHarvester.py -d "$_" -b linkedin > "companies/$_.txt"`;
     }
+    $i++;
 }
 
 # 205 files grabbed via primary
